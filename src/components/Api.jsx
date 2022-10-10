f1fe9a37561813e21bd7c930f22e8bc64807dd89
Juan Mendez <jmendez16@cuc.edu.co>
2022-10-10 10:41:41 -0500

    Nueva Api, funcional

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -2,28 +2,40 @@ import React from 'react'
 
 const Api = () => {
     const [personajes, setPersonajes] = React.useState([])
+    const [paginacion, setPaginacion] = React.useState(1)
 
     const obtenerPersonajes = async () => {
 
-        try {
-            const res = await fetch('https://rickandmortyapi.com/api/character')
+        try {            
+            const res = await fetch(`https://randomuser.me/api/?page=${paginacion}&results=20&seed=foobar`)
             const { results } = await res.json();
             setPersonajes(results)
-            console.log(results)
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const siguientePagina = async () => {
+        await setPaginacion(paginacion + 1);
+        obtenerPersonajes();
+    }
+
+    const retornarPagina = async () => {
+        await setPaginacion(paginacion - 1);
+        obtenerPersonajes();
     }
     return (
         <div>
             <h1>Prueba</h1>
             <button onClick={obtenerPersonajes}>Acción</button>
+            <button onClick={siguientePagina}>Siguiente</button>
+            <button onClick={retornarPagina}>Atrás</button>
             {
-                personajes.map(({ id, name, image, status }) => (
-                    <div key={id}>
-                        <h4 key={id}>{id} - {name}</h4>
-                        <img src={image} alt={name} />
-                        <h5 key={id}>{status}</h5>
+                personajes.map(({ name, picture, gender, email }) => (
+                    <div id='contenedor'>
+                        <h4>{name.first} - {email}</h4>
+                        <img src={picture.medium} alt={name.first} />
+                        <h5>{gender}</h5>                        
                     </div>
                 ))
             }
